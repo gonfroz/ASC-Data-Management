26009ea501a7da691b7291c06ea760b44f3d5fd7
E540 <E540@THECHOSENONE>
2017-03-01 18:56:21 -0600

    Add project files.

---
+++ WindowsFormsApplication2/Form4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Form frm in Application.OpenForms)
+            {
+                if (frm is Form2)
+                {
+                    frm.Show();
+                }
+            }
+            this.Close();
+        }
+    }
+}
