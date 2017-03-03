423bb8c5ab1a77adcdb7356f46f91202936cb045
gonfroz <connorstewartmanning@letu.edu>
2017-03-03 17:03:58 -0600

    Changed variable names
    Variable names should be more recognizable for easier programming in the next phase.

--- WindowsFormsApplication2/Checkout.Designer.cs
+++ WindowsFormsApplication2/Checkout.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication2
+namespace WindowsFormsApplication2
 {
     partial class Checkout
     {
@@ -28,33 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label9 = new System.Windows.Forms.Label();
+            this.GamesLabel = new System.Windows.Forms.Label();
             this.listBox2 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.IDLabel = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.XBoxRadio = new System.Windows.Forms.RadioButton();
+            this.WiiRadio = new System.Windows.Forms.RadioButton();
+            this.PSRadio = new System.Windows.Forms.RadioButton();
+            this.ControllersNum = new System.Windows.Forms.NumericUpDown();
+            this.ControllersLabel = new System.Windows.Forms.Label();
+            this.ConsoleLabel = new System.Windows.Forms.Label();
+            this.GamesList = new System.Windows.Forms.CheckedListBox();
+            this.BackButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ControllersNum)).BeginInit();
             this.SuspendLayout();
             // 
-            // label9
+            // GamesLabel
             // 
-            this.label9.BackColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(350, 22);
-            this.label9.MinimumSize = new System.Drawing.Size(50, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(137, 20);
-            this.label9.TabIndex = 16;
-            this.label9.Text = "Items Ready For Checkout";
-            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.GamesLabel.BackColor = System.Drawing.Color.White;
+            this.GamesLabel.Location = new System.Drawing.Point(350, 22);
+            this.GamesLabel.MinimumSize = new System.Drawing.Size(50, 0);
+            this.GamesLabel.Name = "GamesLabel";
+            this.GamesLabel.Size = new System.Drawing.Size(137, 20);
+            this.GamesLabel.TabIndex = 16;
+            this.GamesLabel.Text = "Items Ready For Checkout";
+            this.GamesLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // listBox2
             // 
@@ -64,110 +65,110 @@
             this.listBox2.Size = new System.Drawing.Size(462, 121);
             this.listBox2.TabIndex = 19;
             // 
-            // textBox1
+            // NameBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(131, 121);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(117, 20);
-            this.textBox1.TabIndex = 20;
+            this.NameBox.Location = new System.Drawing.Point(131, 121);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(117, 20);
+            this.NameBox.TabIndex = 20;
             // 
-            // textBox2
+            // IDBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(131, 150);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(117, 20);
-            this.textBox2.TabIndex = 21;
+            this.IDBox.Location = new System.Drawing.Point(131, 150);
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(117, 20);
+            this.IDBox.TabIndex = 21;
             // 
-            // label1
+            // NameLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 124);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 22;
-            this.label1.Text = "Student Name";
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(50, 124);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(75, 13);
+            this.NameLabel.TabIndex = 22;
+            this.NameLabel.Text = "Student Name";
             // 
-            // label2
+            // IDLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 153);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
-            this.label2.TabIndex = 23;
-            this.label2.Text = "ID Number";
+            this.IDLabel.AutoSize = true;
+            this.IDLabel.Location = new System.Drawing.Point(67, 153);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(58, 13);
+            this.IDLabel.TabIndex = 23;
+            this.IDLabel.Text = "ID Number";
             // 
-            // button1
+            // SubmitButton
             // 
-            this.button1.Location = new System.Drawing.Point(412, 172);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 24;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.SubmitButton.Location = new System.Drawing.Point(412, 172);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 24;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // radioButton1
+            // XBoxRadio
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(53, 45);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(48, 17);
-            this.radioButton1.TabIndex = 25;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "xBox";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.XBoxRadio.AutoSize = true;
+            this.XBoxRadio.Location = new System.Drawing.Point(53, 45);
+            this.XBoxRadio.Name = "XBoxRadio";
+            this.XBoxRadio.Size = new System.Drawing.Size(48, 17);
+            this.XBoxRadio.TabIndex = 25;
+            this.XBoxRadio.TabStop = true;
+            this.XBoxRadio.Text = "xBox";
+            this.XBoxRadio.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // WiiRadio
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(53, 68);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(40, 17);
-            this.radioButton2.TabIndex = 26;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Wii";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.WiiRadio.AutoSize = true;
+            this.WiiRadio.Location = new System.Drawing.Point(53, 68);
+            this.WiiRadio.Name = "WiiRadio";
+            this.WiiRadio.Size = new System.Drawing.Size(40, 17);
+            this.WiiRadio.TabIndex = 26;
+            this.WiiRadio.TabStop = true;
+            this.WiiRadio.Text = "Wii";
+            this.WiiRadio.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // PSRadio
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(53, 91);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(76, 17);
-            this.radioButton3.TabIndex = 27;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Playstation";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.PSRadio.AutoSize = true;
+            this.PSRadio.Location = new System.Drawing.Point(53, 91);
+            this.PSRadio.Name = "PSRadio";
+            this.PSRadio.Size = new System.Drawing.Size(76, 17);
+            this.PSRadio.TabIndex = 27;
+            this.PSRadio.TabStop = true;
+            this.PSRadio.Text = "Playstation";
+            this.PSRadio.UseVisualStyleBackColor = true;
             // 
-            // numericUpDown1
+            // ControllersNum
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(158, 42);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
-            this.numericUpDown1.TabIndex = 28;
+            this.ControllersNum.Location = new System.Drawing.Point(158, 42);
+            this.ControllersNum.Name = "ControllersNum";
+            this.ControllersNum.Size = new System.Drawing.Size(54, 20);
+            this.ControllersNum.TabIndex = 28;
             // 
-            // label3
+            // ControllersLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(146, 26);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 13);
-            this.label3.TabIndex = 29;
-            this.label3.Text = "# of Controllers";
+            this.ControllersLabel.AutoSize = true;
+            this.ControllersLabel.Location = new System.Drawing.Point(146, 26);
+            this.ControllersLabel.Name = "ControllersLabel";
+            this.ControllersLabel.Size = new System.Drawing.Size(78, 13);
+            this.ControllersLabel.TabIndex = 29;
+            this.ControllersLabel.Text = "# of Controllers";
             // 
-            // label4
+            // ConsoleLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(50, 26);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 13);
-            this.label4.TabIndex = 30;
-            this.label4.Text = "Console";
+            this.ConsoleLabel.AutoSize = true;
+            this.ConsoleLabel.Location = new System.Drawing.Point(50, 26);
+            this.ConsoleLabel.Name = "ConsoleLabel";
+            this.ConsoleLabel.Size = new System.Drawing.Size(45, 13);
+            this.ConsoleLabel.TabIndex = 30;
+            this.ConsoleLabel.Text = "Console";
             // 
-            // checkedListBox1
+            // GamesList
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
+            this.GamesList.FormattingEnabled = true;
+            this.GamesList.Items.AddRange(new object[] {
             "Donkey Kong",
             "Fifa",
             "Smash Bros",
@@ -176,53 +177,66 @@
             "Call of Duty",
             "Mario Kart",
             "Battle Toads"});
-            this.checkedListBox1.Location = new System.Drawing.Point(350, 47);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(137, 124);
-            this.checkedListBox1.TabIndex = 31;
+            this.GamesList.Location = new System.Drawing.Point(350, 47);
+            this.GamesList.Name = "GamesList";
+            this.GamesList.Size = new System.Drawing.Size(137, 124);
+            this.GamesList.TabIndex = 31;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BackButton.Location = new System.Drawing.Point(12, 356);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(75, 23);
+            this.BackButton.TabIndex = 32;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
             // Checkout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(513, 353);
-            this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(513, 391);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.GamesList);
+            this.Controls.Add(this.ConsoleLabel);
+            this.Controls.Add(this.ControllersLabel);
+            this.Controls.Add(this.ControllersNum);
+            this.Controls.Add(this.PSRadio);
+            this.Controls.Add(this.WiiRadio);
+            this.Controls.Add(this.XBoxRadio);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.IDLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.IDBox);
+            this.Controls.Add(this.NameBox);
             this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.label9);
+            this.Controls.Add(this.GamesLabel);
             this.Name = "Checkout";
             this.Text = "Checkout";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ControllersNum)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label GamesLabel;
         private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label IDLabel;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.RadioButton XBoxRadio;
+        private System.Windows.Forms.RadioButton WiiRadio;
+        private System.Windows.Forms.RadioButton PSRadio;
+        private System.Windows.Forms.NumericUpDown ControllersNum;
+        private System.Windows.Forms.Label ControllersLabel;
+        private System.Windows.Forms.Label ConsoleLabel;
+        private System.Windows.Forms.CheckedListBox GamesList;
+        private System.Windows.Forms.Button BackButton;
     }
 }
